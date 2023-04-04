43783a084c7d30054f4352b1073460284193a6b8
Joseph Miller <millerj@pixia.com>
2023-04-04 03:57:31 -0400

    link and graphic

---
+++ src/community/web-linking-mangling/src/main/java/org/geoserver/linking/mangling/web/WebLinkingManglingConfigPage.java
@@ -0,0 +1,20 @@
+/* (c) 2023 Open Source Geospatial Foundation - all rights reserved
+ * This code is licensed under the GPL 2.0 license, available at the root
+ * application directory.
+ */
+package org.geoserver.linking.mangling.web;
+
+import org.geoserver.web.GeoServerSecuredPage;
+
+import java.util.Optional;
+
+public class WebLinkingManglingConfigPage extends GeoServerSecuredPage {
+
+
+    public WebLinkingManglingConfigPage() {
+
+    }
+
+
+
+}
