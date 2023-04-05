712473b208085e18188ea6c228f72d4fbe7b88c7
Joseph Miller <millerj@pixia.com>
2023-04-05 06:54:01 -0400

    mangler first pass

--- src/community/web-linking-mangling/src/main/java/org/geoserver/linking/mangling/WebLinkingUrlMangler.java
+++ src/community/web-linking-mangling/src/main/java/org/geoserver/linking/mangling/WebLinkingUrlMangler.java
@@ -6,14 +6,17 @@ import org.geoserver.linking.mangling.config.LinkingManglerRuleDAO;
 import org.geoserver.ows.HTTPHeadersCollector;import org.geoserver.ows.ProxifyingURLMangler;import org.geoserver.ows.URLMangler;
 import org.geoserver.platform.resource.Resource;
 import org.geotools.util.logging.Logging;
-import java.util.Arrays;import java.util.HashMap;import java.util.List;import java.util.Map;import java.util.Optional;import java.util.logging.Logger;
+import java.net.MalformedURLException;import java.net.URL;import java.util.Arrays;import java.util.HashMap;import java.util.List;import java.util.Map;import java.util.Optional;import java.util.logging.Level;import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import static org.geoserver.linking.mangling.config.LinkingManglerRuleDAO.getLinkingManglerRules;
 
 public class WebLinkingUrlMangler implements URLMangler {
     private static final Logger LOGGER = Logging.getLogger(WebLinkingUrlMangler.class);
-    private static Pattern TEMPLATE_LITERAL_PATTERN = Pattern.compile("\\$\\{(.*?)\\}");
+    private static final Pattern TEMPLATE_LITERAL_PATTERN = Pattern.compile("\\$\\{(.*?)\\}");
+    public static final String TEMPLATE_PREFIX = "${";
+    public static final String TEMPLATE_POSTFIX = "}";
+    public static final String PROTOCOL_SEPARATOR = "://";
 
     protected List<LinkingManglerRule> linkingManglerRules;
     public WebLinkingUrlMangler(GeoServerDataDirectory dataDirectory) {
@@ -33,19 +36,44 @@ public class WebLinkingUrlMangler implements URLMangler {
                     .map(matchResult -> matchResult.group(1)).collect(Collectors.toList());
             if(templateLiterals.isEmpty()) {
                 //if there are no template literals, just replace the url with the transformer
-                transformURL(baseURL,path, transformer);
+                transformURL(baseURL,path, transformer.get());
                 return;
             }
             //if there are template literals, collect the headers
             Map<String,String> headers = collectHeaders(templateLiterals);
             //check if headers are missing
             if(headers.size() != templateLiterals.size()) {
-                LOGGER.info("Web Linking URL Mangler: Some headers are missing, cannot transform the URL using the transformer: " + transformer.get() );
+                LOGGER.info("Web Linking URL Mangler: Some headers are missing, cannot transform the URL using the transformer: "
+                        + transformer.get() );
                 return;
             }
-            transformURL(baseURL,path, transformer, headers);
+            transformURL(baseURL,path, transformer.get(), headers);
         }
 
+    }
+    private void transformURL(StringBuilder baseURL, StringBuilder path, String transformer, Map<String,String> headers) {
+        String transformerReplacedLiterals = transformer;
+        if(headers != null) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                transformerReplacedLiterals = transformerReplacedLiterals.replace(TEMPLATE_PREFIX + entry.getKey()
+                        + TEMPLATE_POSTFIX, entry.getValue());
+            }
+        }
+        try{
+            URL url = new URL(transformerReplacedLiterals);
+            baseURL.setLength(0);
+            baseURL.append( url.getProtocol() + PROTOCOL_SEPARATOR + url.getHost());
+            path.setLength(0);
+            path.append(url.getPath());
+        }catch (MalformedURLException e) {
+            LOGGER.log(Level.ALL,"Web Linking URL Mangler: The transformer, after header template replacement: "
+                    + transformerReplacedLiterals + " is not a valid URL", e);
+            return;
+        }
+
+    }
+    private void transformURL(StringBuilder baseURL, StringBuilder path, String transformer) {
+        transformURL(baseURL, path, transformer, null);
     }
 
     private Optional<String> getFirstMatchingTransformer(String path) {
