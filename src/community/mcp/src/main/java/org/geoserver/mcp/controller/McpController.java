6ea75c5b3cf0e3589c38483011169937dfcd4dde
Joseph Miller <millerjoseph@gmail.com>
2026-05-21 15:11:09 -0400

    [GEOS-12117] GeoServer MCP Community Module

--- src/community/mcp/src/main/java/org/geoserver/mcp/controller/McpController.java
+++ src/community/mcp/src/main/java/org/geoserver/mcp/controller/McpController.java
@@ -1,21 +1,20 @@
 package org.geoserver.mcp.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import org.geoserver.config.GeoServer;
+import org.geoserver.rest.RestBaseController; // Core GeoServer rest base class
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class McpController  {
+public class McpController extends RestBaseController {
+    @Autowired
+    public McpController(GeoServer geoServer) {
+        super(); // <--- PUT A BREAKPOINT HERE
+    }
 
-    @GetMapping(
-            produces = "application/json",
-            path = "/mcp",
-            name = "MCPTest")
-    @ResponseBody
-    public ExportMap exportMap(@PathVariable String workspaceName, HttpServletRequest request) {
-        String requestURL = request.getRequestURL().toString();
-        String requestParameters = request.getQueryString();
-        String updatedRequestParameters = requestParameters.replaceAll("\\bf=[^&]+", "f=image&format=png");
-
-        return new ExportMap(requestURL + updatedRequestParameters);
+    @GetMapping(produces = "application/json", path = RestBaseController.ROOT_PATH + "/mcp", name = "MCPTest")
+    public String exportMap() {
+        return "hello world";
     }
 }
